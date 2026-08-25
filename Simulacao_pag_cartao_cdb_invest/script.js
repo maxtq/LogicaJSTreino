f943e5eb65cc07565b6d9b5a10085ffc832003f4
maxtq <maxtquaglia@gmail.com>
2026-08-25 08:38:11 -0300

    progresso mas bugado

--- Simulacao_pag_cartao_cdb_invest/script.js
+++ Simulacao_pag_cartao_cdb_invest/script.js
@@ -5,3 +5,43 @@ const dados = [
         ibxr100: 0
     }
 ];
+
+const corpoTabela = document.getElementById("tabela");
+
+function preencherTabela(dados) {
+    // Limpa a tabela antes de preencher
+    corpoTabela.innerHTML = "";
+
+    dados.forEach(obj => {
+        const tr = document.createElement("tr");
+
+        // Pega cada valor do objeto
+        Object.values(obj).forEach(valor => {
+            const td = document.createElement("td");
+            td.textContent = valor;
+            tr.appendChild(td);
+        });
+
+        corpoTabela.appendChild(tr);
+    });
+}
+
+function crescimento_divida() {
+    const Rngtakediv = Math.random() * 1000;
+
+    if (Rngtakediv >= 500) {
+
+        const valordiv = Math.random();
+        const novaDivida = valordiv * 1000;
+
+        dados.push({
+            divida: novaDivida + dados.divida,
+            cdb: (novaDivida / 2) + dados.cdb,
+            ibxr100: (novaDivida / 2) + dados.ibrx100
+        });
+        preencherTabela(dados);
+    }
+}
+
+const interval = setInterval(crescimento_divida,2500);
+preencherTabela(dados);
