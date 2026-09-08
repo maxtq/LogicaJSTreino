cb4bb46ae0614c39f414f9a7d0ad463b8dc1c044
maxtq <83239706+maxtq@users.noreply.github.com>
2026-09-08 11:00:44 -0300

    Update script.js

--- Simulacao_pag_cartao_cdb_invest/script.js
+++ Simulacao_pag_cartao_cdb_invest/script.js
@@ -1,3 +1,4 @@
+// valores que serao mostrados no html
 const dados = [
     {
         mes: 0,
@@ -6,6 +7,15 @@ const dados = [
         ibxr100: 0
     }
 ];
+
+
+// valores que serão guardados e não será mostrados no html
+const privdados = [
+    {
+        parcelasVal: 0,
+        parcelasquant: 0
+    }
+]
 
 const corpoTabela = document.getElementById("tabela");
 
@@ -27,33 +37,51 @@ function preencherTabela(dados) {
     });
 }
 
+// funcao que cria uma compra aleatoria nova com 50% de chance de ter uma divida nova de 0 a 1000
 function crescimento_divida() {
+
+    // gera numero aleatorio
     const Rngtakediv = Math.random() * 1000;
 
     if (Rngtakediv >= 500) {
 
-        const valordiv = Math.random();
-        const novaDivida = dados[dados.length-1].divida + valordiv * 1000;
+        const valordiv = Math.random() * 1000;
+        const novaDivida = dados[dados.length-1].divida + valordiv;
 
         dados.push({
             mes: dados[dados.length-1].mes + 1,
-            divida: novaDivida,
-            cdb: novaDivida,
-            ibxr100: novaDivida
+            divida: novaDivida
         });
         preencherTabela(dados);
+
+
     }else{
     
     novaDivida = dados[dados.length-1].divida
 
     dados.push({
         mes: dados[dados.length-1].mes + 1,
-        divida: novaDivida,
-        cdb: novaDivida,
-        ibxr100: novaDivida
+        divida: novaDivida
     });
     preencherTabela(dados);
     }
+    
+    crescimento_parcelas
+}
+
+// funcao que simula a quantidade de parcelas e o valor delas
+function crescimento_cdb() {
+    
+}
+
+// funcao que simula quanto dinheiro sera colocado no cdb para
+function crescimento_cdb() {
+    
+}
+
+// funcao que simula quanto dinheiro sera colocado no ibrx 100
+function crescimento_ibrx100() {
+    
 }
 
 const interval = setInterval(crescimento_divida,2500);
